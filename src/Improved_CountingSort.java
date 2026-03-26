6e3d9c9a113207cbf020118843be791d8975afb7
zij3 <james230706@gmail.com>
2026-03-26 11:51:19 +0000

    Partition method

--- src/Improved_CountingSort.java
+++ src/Improved_CountingSort.java
@@ -1,11 +1,11 @@
+import java.util.Arrays;
+
 public class Improved_CountingSort {
     private static final int C = 1000;
 
-    private static void quicksort_modified(int arr[], int low, int high,
+    private static void quicksort_modified(int[] arr, int low, int high,
                             int maxValue, int minValue) {
-        while ((low < high) &&
-                (maxValue - minValue +
-                        high - low > C)) {
+        while ((low < high) && (maxValue - minValue + high - low > C)) {
             int pivot = partition(arr, low, high);
             int midValue = arr[pivot];
             quicksort_modified(arr, low,
@@ -18,8 +18,29 @@ public class Improved_CountingSort {
     }
 
     private static int partition(int[] arr, int low, int high) {
-        return 0;
+        int[] arr1 = Arrays.copyOf(arr,arr.length);
+        int pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr1[j] < pivot) {
+                i++;
+                swap(arr1, i, j);
+            }
+        }
+
+        swap(arr1, i + 1, high);
+        return i + 1;
     }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
 
     private static int getMax(int[]arr, int n) {
         int max = arr[0];
@@ -47,5 +68,14 @@ public class Improved_CountingSort {
         }
         for (int i = 0; i < n; i++)
             arr[i] = output[i];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 10000000, 3, 2, 4};
+        quicksort_modified(arr, 0, 4, getMax(arr, arr.length), 1);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
